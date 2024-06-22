0048682fbb8068ca4db2d37e559a9fb4cd95888d
ki1lmyself <sofiamiklzkova@gmail.com>
2024-06-22 19:31:29 +0300

    Добавьте файлы проекта.

---
+++ LabWork48/DataAccessLayer.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork48
+{
+    public static class DataAccessLayer
+    {
+        public static string Server { get; set; } = @"prserver\SQLEXPRESS";
+        public static string Login { get; set; } = "ispp2104";
+        public static string Password { get; set; } = "2104";
+        public static string Database { get; set; } = "ispp2104";
+        public static bool TrustServerCertificate { get; set; } = true;
+
+        public static string ConnectionString
+        {
+            get
+            {
+                SqlConnectionStringBuilder builder = new()
+                {
+                    DataSource = Server,
+                    UserID = Login,
+                    Password = Password,
+                    InitialCatalog = Database,
+                    TrustServerCertificate = true,
+                };
+                return builder.ConnectionString;
+            }
+        }
+
+        public static void AddAuthor(string surname, string name, string country)
+        {
+            using SqlConnection connection = new(ConnectionString);
+            connection.Open();
+
+            using SqlCommand command = new("AddAuthor", connection);
+
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@surname", surname);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@country", country);
+            command.ExecuteNonQuery();
+        }
+
+        public static int GetAuthorId(string surname, string name, string country)
+        {
+            using SqlConnection connection = new(ConnectionString);
+            connection.Open();
+
+            using SqlCommand command = new("AddAuthorId", connection);
+
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@surname", surname);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@country", country);
+            return Convert.ToInt32(command.ExecuteScalar());
+        }
+
+        //Task4
+        public static DataTable GetBookPrice()
+        {
+            using SqlConnection connection = new(ConnectionString);
+            connection.Open();
+
+            using SqlCommand command = new("AddAuthorId", connection);
+
+        }
+    }
+}
---
+++ LabWork48/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Windows;
+
+namespace LabWork48
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddAuthorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (nameTextBox.Text != null || surnameTextBox.Text != null || countryTextBox.Text != null)
+            {
+                try
+                {
+                    DataAccessLayer.AddAuthor(nameTextBox.Text, surnameTextBox.Text, countryTextBox.Text);
+                    MessageBox.Show($"{nameTextBox.Text} {surnameTextBox.Text} успешно добавлен в БД");
+
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось выполнить запрос");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Заполните данные");
+            }
+        }
+
+        private void AddAuthorIdButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (surnameTextBox.Text != null || nameTextBox.Text != null || countryTextBox.Text != null)
+                {
+                    MessageBox.Show($"{DataAccessLayer.GetAuthorId(nameTextBox.Text, surnameTextBox.Text, countryTextBox.Text)} {nameTextBox.Text} {surnameTextBox.Text} успешно добавлен в БД");
+                }
+                else
+                {
+                    MessageBox.Show("Заполните данные");
+                }
+               
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось выполнить запрос");
+            }
+        }
+    }
+}
